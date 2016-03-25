c4b83330de29dd2183de32ab4fb70d0998da0a71
Chris Maher <chrismaher.wit@gmail.com>
2016-03-25 20:38:21 +0000

    Start Brewing...

---
+++ js/app/popup.js
@@ -0,0 +1,20 @@
+﻿myApp.controller("PageController", function ($scope, $http) {
+
+
+    $http.get('http://www.coffeemate.club/api/coffees').success(function (data) {
+        $scope.coffees = data;
+
+        for (i = 0; i < $scope.coffees.length; i++) {
+            if ($scope.coffees[i].title.length > 32) {
+                $scope.coffees[i].title = $scope.coffees[i].title.slice(0,32);
+            }
+
+            $scope.coffees[i].title = $scope.coffees[i].title.toUpperCase();
+        }
+
+    });
+
+});
+
+
+
